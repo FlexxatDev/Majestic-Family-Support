0453ae30792f2fa5415e13d9c0983001a937362f
Denis <145290547+FlexxatDev@users.noreply.github.com>
2024-08-19 03:39:23 +0300

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -2,24 +2,56 @@ import disnake
 from disnake.ext import commands
 from typing import Optional
 import os
-log_contracts = None #id канала в который будут приходить логиконтрактов
+from Config import *
+
 
 
 bot = commands.Bot(command_prefix="!", help_command=None, intents=disnake.Intents.all(), test_guilds=[1273029650464903350])
 
-class Register(disnake.ui.View):
+class Fraction(disnake.ui.View):
+    
+    def __init__(self):
+        super().__init__(timeout=None)
+        self.value = Optional[bool]
+
+    @disnake.ui.button(label="Крайм", style=disnake.ButtonStyle.blurple, emoji = "👓")
+    async def Crime(self, button:disnake.ui.button, inter: disnake.MessageInteraction):
+        global Frac
+        await inter.send("Выберите ветку контракта", view=ContractsCrime())
+        self.value = True
+        Frac = "Crime"
+    @disnake.ui.button(label="Госс", style=disnake.ButtonStyle.blurple, emoji = "🦺")
+    async def Farm(self, button:disnake.ui.button, inter: disnake.MessageInteraction):
+        global Frac
+        await inter.send("Выберите ветку контракта", view=ContractsFarm())
+        self.value = True
+        Frac = "Farm"
+
+
+class ContractsFarm(disnake.ui.View):
     def __init__(self):
         super().__init__(timeout=None)
         self.value = Optional[bool]
 
     @disnake.ui.button(label="Дары моря", style=disnake.ButtonStyle.blurple, emoji = "🐳")
     async def SeaGift(self, button:disnake.ui.button, inter: disnake.MessageInteraction):
-        await inter.send("Выберите тип контракта", ephemeral=True, view=DropdownView())
+        await inter.send("Выберите тип контракта", ephemeral=True, view=SeaGifts())
         self.value = True
     @disnake.ui.button(label="Металлург", style=disnake.ButtonStyle.blurple, emoji = "⛏")
     async def Metal(self, button:disnake.ui.button, inter: disnake.MessageInteraction):
-        await inter.send("Выберите тип контракта", ephemeral=True, view=DropdownView1())
+        await inter.send("Выберите тип контракта", ephemeral=True, view=Metals())
         self.value = True
+
+class ContractsCrime(disnake.ui.View):
+    def __init__(self):
+        super().__init__(timeout=None)
+        self.value = Optional[bool]
+
+    @disnake.ui.button(label="Гровер", style=disnake.ButtonStyle.blurple, emoji = "🌱")
+    async def Grover(self, button:disnake.ui.button, inter: disnake.MessageInteraction):
+        await inter.send("Выберите тип контракта", ephemeral=True, view=Grovers())
+        self.value = True
+
 
 class ContractLogAccept(disnake.ui.View):
     def __init__(self):
@@ -32,15 +64,18 @@ class ContractLogAccept(disnake.ui.View):
         for i in member.split():
             try:
                 if choice == 1:
-                    summ, comment = await Dropdown.returnsumm()
+                    summ, comment = await Dropdown.returnsumm(self, inter)
                 elif choice == 2:
-                    summ, comment = await Dropdown1.returnsumm()
+                    summ, comment = await Dropdown1.returnsumm(self, inter)
+                elif choice == 3:
+                    summ, comment = await Dropdown2.returnsumm(self, inter)    
                 static = int(i)
                 File = open("File.txt", "a+")
                 File.write(f"{static};{summ};{comment}\n")
                 File.close()
             except:
                 pass
+                
         self.value = True
         self.stop()
 
@@ -67,7 +102,47 @@ class Delete(disnake.ui.View):
 class Dropdown1(disnake.ui.StringSelect):
     def __init__(self):
         options = [
-            disnake.SelectOption(label="1 Металлург (Выплата 25.000$)", description="Контракт доступен раз в 24 часа")
+            disnake.SelectOption(label=f"1Металлургия, Выплата: {Metal1}", description="Выполни контракт что бы получить награду"),
+            disnake.SelectOption(label=f"2Металлургия, Выплата: {Metal2}", description="Выполни контракт что бы получить награду"),
+            disnake.SelectOption(label=f"3Металлургия, Выплата: {Metal3}", description="Выполни контракт что бы получить награду"),
+            disnake.SelectOption(label=f"4Металлургия, Выплата: {Metal4}", description="Выполни контракт что бы получить награду"),
+        ]
+        super().__init__(
+            placeholder = "MENU",
+            min_values = 1,
+            max_values = 1,
+            options = options 
+        )
+    async def callback(self, inter: disnake.MessageInteraction):
+        global choice
+        global member
+        global contract 
+        choice = 2
+        member = inter.user.display_name
+        contract = inter.data["values"][0]
+        await ContarctLog(member, contract)
+        await inter.response.send_message(f"Отчет на {self.values[0]} отправлен!", ephemeral=True)
+    async def returnsumm(self, inter: disnake.MessageInteraction):
+        for i in contract.split():
+            try:
+                money = int(i)
+                return(money, f"{contract}")
+            except:
+                pass
+
+class Metals(disnake.ui.View):
+    def __init__(self):
+        super().__init__()
+        self.add_item(Dropdown1())
+
+class Dropdown2(disnake.ui.StringSelect):
+    def __init__(self):
+        options = [
+            disnake.SelectOption(label=f"1Гровер, Выплата: {Grover1}", description="Выполни контракт что бы получить награду"),
+            disnake.SelectOption(label=f"2Гровер, Выплата: {Grover2}", description="Выполни контракт что бы получить награду"),
+            disnake.SelectOption(label=f"3Гровер, Выплата: {Grover3}", description="Выполни контракт что бы получить награду"),
+            disnake.SelectOption(label=f"4Гровер, Выплата: {Grover4}", description="Выполни контракт что бы получить награду"),
+            disnake.SelectOption(label=f"5Гровер, Выплата: {Grover5}", description="Выполни контракт что бы получить награду")
         ]
         super().__init__(
             placeholder = "MENU",
@@ -78,23 +153,36 @@ class Dropdown1(disnake.ui.StringSelect):
     async def callback(self, inter: disnake.MessageInteraction):
         global choice
         global member 
-        choice = 2
+        global contract
+        choice = 3
         member = inter.user.display_name
         contract = inter.data["values"][0]
         await ContarctLog(member, contract)
         await inter.response.send_message(f"Отчет на {self.values[0]} отправлен!", ephemeral=True)
-    async def returnsumm():
-        return(25000, "Металлург 1 уровень")
+    async def returnsumm(self, inter: disnake.MessageInteraction):
+         for i in contract.split():
+            try:
+                money = int(i)
+                return(money, f"{contract}")
+            except:
+                pass
 
-class DropdownView1(disnake.ui.View):
+class Grovers(disnake.ui.View):
     def __init__(self):
         super().__init__()
-        self.add_item(Dropdown1())
+        self.add_item(Dropdown2())
 
 class Dropdown(disnake.ui.StringSelect):
     def __init__(self):
         options = [
-            disnake.SelectOption(label="1 Уровень Дары моря (Выплата 20.000$)", description="Контракт доступен раз в 2 часа")
+           disnake.SelectOption(label=f"1Дары моря, Выплата: {SeaGift1}", description="Выполни контракт что бы получить награду"),
+           disnake.SelectOption(label=f"2Дары моря, Выплата: {SeaGift2}", description="Выполни контракт что бы получить награду"),
+           disnake.SelectOption(label=f"3Дары моря, Выплата: {SeaGift3}", description="Выполни контракт что бы получить награду"),
+           disnake.SelectOption(label=f"4Дары моря, Выплата: {SeaGift4}", description="Выполни контракт что бы получить награду"),
+           disnake.SelectOption(label=f"5Дары моря, Выплата: {SeaGift5}", description="Выполни контракт что бы получить награду"),
+           disnake.SelectOption(label=f"6Дары моря, Выплата: {SeaGift6}", description="Выполни контракт что бы получить награду"),
+           disnake.SelectOption(label=f"7Дары моря, Выплата: {SeaGift7}", description="Выполни контракт что бы получить награду"),
+           disnake.SelectOption(label=f"8Дары моря, Выплата: {SeaGift8}", description="Выполни контракт что бы получить награду")
         ]
         super().__init__(
             placeholder = "MENU",
@@ -105,17 +193,23 @@ class Dropdown(disnake.ui.StringSelect):
     async def callback(self, inter: disnake.MessageInteraction):
         global choice 
         global member
+        global contract
         choice = 1
         member = inter.user.display_name
         contract = inter.data["values"][0]
         await ContarctLog(member, contract)
         await inter.response.send_message(f"Отчет на {self.values[0]} отправлен!", ephemeral=True)
-    async def returnsumm():
-        return(20000, "Дары моря 1 уровень")
+    async def returnsumm(self, inter: disnake.MessageInteraction):
+        for i in contract.split():
+            try:
+                money = int(i)
+                return(money, f"{contract}")
+            except:
+                pass
 
 
 
-class DropdownView(disnake.ui.View):
+class SeaGifts(disnake.ui.View):
     def __init__(self):
         super().__init__()
         self.add_item(Dropdown())
@@ -137,8 +231,8 @@ async def ContarctLog(member, contract):
 @bot.command()
 @commands.has_permissions(administrator=True)
 async def reg(ctx):
-    view = Register()
-    await ctx.send(f"Привет! Выбери контракт для отчета!", view=view)
+    view = Fraction()
+    await ctx.send(f"Привет, выбери ветку свой фамы", view=view)
     
 
 @bot.command()
